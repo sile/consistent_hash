2a3f2c41fbc498c83e60f43c75342a511f9f7098
Takeru Ohta <phjgt308@gmail.com>
2017-01-19 03:45:48 +0900

    Show elapsed time for building hash ring

--- examples/bench.rs
+++ examples/bench.rs
@@ -34,18 +34,20 @@ fn main() {
     println!("WORD COUNT: {}", words.len());
 
     let vnodes = matches.value_of("VNODE_COUNT").unwrap().parse().expect("Wrong integer");
+    let build_start_time = Instant::now();
     let ring = StaticHashRing::new(DefaultHash,
                                    matches.values_of("NODES")
                                        .unwrap()
                                        .map(|n| Node::new(n).quantity(vnodes)));
+    let build_end_time = Instant::now();
     println!("REAL NODE COUNT: {}", ring.nodes().len());
     println!("VIRTUAL NODE COUNT: {} ({} per node)", ring.len(), vnodes);
 
-    let start_time = Instant::now();
+    let select_start_time = Instant::now();
     for word in words.iter() {
         ring.calc_candidates(word).nth(0).unwrap();
     }
-    let end_time = Instant::now();
+    let select_end_time = Instant::now();
 
     let mut counts: HashMap<&str, _> = HashMap::from_iter(ring.nodes().iter().map(|k| (k.key, 0)));
     for word in words.iter() {
@@ -60,9 +62,16 @@ fn main() {
     }
     println!("");
 
-    let elapsed = end_time - start_time;
-    let elapsed_micros = elapsed.as_secs() * 1_000_000 + (elapsed.subsec_nanos() / 1000) as u64;
-    println!("ELAPSED: {} ms", elapsed_micros / 1000);
+    let build_elapsed = build_end_time - build_start_time;
+    let build_elapsed_micros = build_elapsed.as_secs() * 1_000_000 +
+                               (build_elapsed.subsec_nanos() / 1000) as u64;
+
+    let select_elapsed = select_end_time - select_start_time;
+    let select_elapsed_micros = select_elapsed.as_secs() * 1_000_000 +
+                                (select_elapsed.subsec_nanos() / 1000) as u64;
+    println!("ELAPSED: {} ms (for building ring), {} ms (for selecting nodes)",
+             build_elapsed_micros / 1000,
+             select_elapsed_micros / 1000);
     println!("WORDS PER SECOND: {}",
-             (((words.len() as f64) / (elapsed_micros as f64)) * 1_000_000.0) as u64);
+             (((words.len() as f64) / (select_elapsed_micros as f64)) * 1_000_000.0) as u64);
 }

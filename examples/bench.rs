1f7d3dfe6b596ac856d922dd080f4968b46add19
Takeru Ohta <phjgt308@gmail.com>
2017-01-19 03:14:43 +0900

    Add "--vnode_count" command line option

--- examples/bench.rs
+++ examples/bench.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::iter::FromIterator;
 use std::time::Instant;
 use clap::{App, Arg};
-use consistent_hash::{StaticHashRing, Node, DefaultRingHasher};
+use consistent_hash::{StaticHashRing, Node, DefaultHash};
 
 fn main() {
     let matches = App::new("bench")
@@ -20,6 +20,10 @@ fn main() {
             .takes_value(true)
             .min_values(1)
             .multiple(true))
+        .arg(Arg::with_name("VNODE_COUNT")
+            .long("vnode_count")
+            .takes_value(true)
+            .default_value("1000"))
         .get_matches();
 
     let filepath = matches.value_of("WORD_FILE").unwrap();
@@ -29,21 +33,23 @@ fn main() {
         .expect("Cannot read words");
     println!("WORD COUNT: {}", words.len());
 
-    let vnodes = 3000; // TODO: parameter
-    let nodes =
-        StaticHashRing::new(DefaultRingHasher::new(),
-                            matches.values_of("NODES").unwrap().map(|n| Node::new(n, (), vnodes)));
-    println!("NODE COUNT: {}", nodes.len());
+    let vnodes = matches.value_of("VNODE_COUNT").unwrap().parse().expect("Wrong integer");
+    let ring = StaticHashRing::new(DefaultHash,
+                                   matches.values_of("NODES")
+                                       .unwrap()
+                                       .map(|n| Node::new(n).quantity(vnodes)));
+    println!("REAL NODE COUNT: {}", ring.nodes().len());
+    println!("VIRTUAL NODE COUNT: {} ({} per node)", ring.len(), vnodes);
 
     let start_time = Instant::now();
     for word in words.iter() {
-        nodes.calc_candidates(word).nth(0).unwrap();
+        ring.calc_candidates(word).nth(0).unwrap();
     }
     let end_time = Instant::now();
 
-    let mut counts: HashMap<&str, _> = HashMap::from_iter(nodes.iter().map(|k| (k.key, 0)));
+    let mut counts: HashMap<&str, _> = HashMap::from_iter(ring.nodes().iter().map(|k| (k.key, 0)));
     for word in words.iter() {
-        let selected = nodes.calc_candidates(word).nth(0).unwrap();
+        let selected = ring.calc_candidates(word).nth(0).unwrap();
         *counts.get_mut(selected.key).unwrap() += 1;
     }
 
